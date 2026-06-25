bd5ac0f9403998406ade57939e79fd5e88da0b1c
Teerth Shah <shahteerth385@gmail.com>
2026-06-25 14:58:59 +0530

    Enhance comments in singleNumber method
    Added comments to explain the XOR operation for removing duplicates.

--- Arrays/136.SingleNumber.java
+++ Arrays/136.SingleNumber.java
@@ -1,8 +1,17 @@
+## Question: 136.Single Number
+
+### Concept Learned
+- XOR Operator
+
+### What this question taught me
+- How to actually remove duplicates using XOR operator 
+
 class Solution {
     public int singleNumber(int[] nums) {
         int ans =0;
         for (int i = 0 ; i<nums.length;i++){
-            ans ^= nums[i];  
+            ans ^= nums[i];
+            // Values with more than one count will be removed
         }
         return ans ;
     }
